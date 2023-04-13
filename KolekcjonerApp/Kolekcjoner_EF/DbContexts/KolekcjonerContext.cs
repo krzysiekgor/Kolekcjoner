a372ca8f535bace0a3c44d0beb2b3809c59efdd2
Krzysztof Górecki <krzysiekgor94@gmail.com>
2023-04-13 15:23:53 +0200

    New: Dodanie KolekcjonerContext

--- KolekcjonerApp/Kolekcjoner_EF/DbContexts/KolekcjonerContext.cs
+++ KolekcjonerApp/Kolekcjoner_EF/DbContexts/KolekcjonerContext.cs
@@ -1,6 +1,16 @@
-﻿namespace Kolekcjoner_EF.DbContexts;
+﻿using System.Data.Entity;
+using KolekcjonerModel.Models;
+using Collection = KolekcjonerModel.Models.Collection;
 
-public class KolekcjonerContext
+namespace Kolekcjoner_EF.DbContexts;
+
+public class KolekcjonerContext: DbContext
 {
     
+    public DbSet<Collection> Collections { get; set; }
+    public DbSet<CollectionDefinition> Type { get; set; }
+    public DbSet<CollectionPosition> CollectionPositions { get; set; }
+    public DbSet<PositionProperty> PositionProperties { get; set; }
+    public DbSet<Property> Properties { get; set; }
+    public DbSet<PropertyType> PropertyTypes { get; set; }
 }
